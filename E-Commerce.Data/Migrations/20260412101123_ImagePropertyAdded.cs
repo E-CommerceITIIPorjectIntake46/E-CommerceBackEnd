b0b9818504540679d93cdc958b5af8ac9edf910f
AbdallahEld <abdallaheldesoky301@gmail.com>
2026-04-12 12:15:43 +0200

    Database Tables Product and Category Updated

---
+++ E-Commerce.Data/Migrations/20260412101123_ImagePropertyAdded.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_Commerce.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImagePropertyAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Categories");
+        }
+    }
+}
